e155132646440082c0ada951148a3d10be84b39e
unknown6656 <unknown84@gmx.de>
2022-02-11 11:27:07 +0100

    updated caller script

--- publish.bat
+++ publish.bat
@@ -1,2 +1,11 @@
 @echo off
-powershell ./publish.ps1
+IF EXIST publish.py (
+    python publish.py "%~f0"
+) ELSE (
+    IF EXIST ../Unknown6656.Publisher/publish.py (
+        python ../Unknown6656.Publisher/publish.py "%~f0"
+    ) ELSE (
+        echo "The file 'publish.py' does not seem to exist in the current dir or inside of '../Unknown6656.Publisher'."
+        echo "Please clone 'https://github.com/Unknown6656-Megacorp/Unknown6656.Publisher' into the current or corresponding directoy."
+    )
+)
